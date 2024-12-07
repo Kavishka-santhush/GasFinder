38760ecc989d579f5756978ca379437c788e1d27
Kavishka-santhush <104711168+Kavishka-santhush@users.noreply.github.com>
2024-12-07 14:25:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -68,7 +68,7 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDqwUhD7KEMsSpLYAMgTkTg-45EZOVAcD8&libraries=places"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_GOOGLE_MAP_API_KEY&libraries=places"></script>
     <script src="js/main.js"></script>
 </body>
 </html>
